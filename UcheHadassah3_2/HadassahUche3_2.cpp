08fa308ed6c98b01e923cde3bb73bc60316e1dc8
Hadassah Uche <uhadassah1@gmail.com>
2023-11-03 11:24:18 +0200

    Created a rundown for the code. There are some bugs tho

---
+++ UcheHadassah3_2/HadassahUche3_2.cpp
@@ -0,0 +1,32 @@
+/*Separating digits: Write a program that takes an integer, stores its digits into an 
+array, and then prints the array with spaces separating the digits. Test your function.
+Example run (user response in bold): 
+Enter a number: 3456
+The number with spaces is: 3 4 5 6 
+HINT: Use the process of separating digits, as in Exercise Sheet 2, Question 4. */
+#include<iostream>
+using namespace std;
+int main()
+{
+	const int MAX = 100;
+	int num;
+	int Array[100];
+	int i = 0;
+	cout << "Enter an integer:";
+	cin >> num;
+	while(num % 10 != 0)
+	{
+		for (; i < MAX; i++)//Saves the number in reverse
+		{
+			Array[i] = num % 10;
+			num = num / 10;
+		}
+		Array[i] = num;
+	}
+	cout << "The number with spaces is:";
+	for (int j = i; j >= 0; j--)
+	{
+		cout << Array[j] << " ";
+	}
+	return 0;
+}
